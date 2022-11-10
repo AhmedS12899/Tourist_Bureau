3c612c8ae3bd4d7acb82b3ad00f8ae897e0f38d2
AhmedS12899 <AhmedS12899@Gmail.com>
2022-11-10 08:55:39 -0500

    updated workshop

---
+++ scripts/activities.js
@@ -0,0 +1,251 @@
+"use strict";
+
+let categories = ["Adventures", "Arts & Crafts", "Museums", "Wine Tastings", "Other"];
+
+let activities = [
+    {
+        category: "Adventures",
+        id: "A101",
+        name: "Valley Hot Air Balloons",
+        description: "Enjoy a lovely hot air balloon ride over the valley at sunrise.  Call 800-555-1212 to reserve a date/time after you complete your e-ticket purchase.",
+        location: "121 S. Main Street",
+        price: 265.00
+    },
+    {
+        category: "Adventures",
+        id: "A102",
+        name: "River Runners: Float Trip",
+        description: "A mellow float trip with lovely scenery, great fishing, just a few riffles, and it finishes back at our base. It is a perfect trip for those wishing to take their time, or those on a limited schedule.",
+        location: "145 FM 103",
+        price: 65.00
+    },
+    {
+        category: "Adventures",
+        id: "A103",
+        name: "River Runners: Ride the Rapids",
+        description: "Experience 3-4 hours of Class II and III whitewater rafting with breathtaking scenery. It is a fun, thrilling, and memorable adventure that everyone from ages 8 and up can enjoy – no experience necessary!",
+        location: "145 FM 103",
+        price: 145.00
+    },
+    {
+        category: "Arts & Crafts",
+        id: "AC101",
+        name: "Painting with a Twist : Oils",
+        description: "Enjoy 2 hours of creating an oil painting by following along with an experienced artist.  Drinks and snacks are included.",
+        location: "1991 Paint Drive",
+        price: 40.00
+    },
+    {
+        category: "Arts & Crafts",
+        id: "AC102",
+        name: "Painting with a Twist : Watercolor",
+        description: "Enjoy 2 hours of creating a watercolor painting by following along with an experienced artist.  Drinks and snacks are included.",
+        location: "1991 Paint Drive",
+        price: 40.00
+    },
+    {
+        category: "Museums",
+        id: "M101",
+        name: "Bravings Airship Museum",
+        description: "Enjoy climbing on and in our collection of small airplanes.  You will find bi-planes, experimental planes and small jets.",
+        location: "101 Airfield Drive",
+        price: 10.00
+    },
+    {
+        category: "Museums",
+        id: "M102",
+        name: "Forks and Spoons Museum",
+        description: "Enjoy touring our qwerky Forks and Spoons Museum.  It houses the worlds largest collection of, you guessed it, forks and spoons!",
+        location: "1056 Lost Knives Court",
+        price: 3.00
+    },
+    {
+        category: "Museums",
+        id: "M103",
+        name: "Steenges Computing Museum",
+        description: "Enjoy our the Stengees Computing Museum that illustrates how computing has changed over the last 60 years.",
+        location: "103 Technology Way",
+        price: 0.00
+    },
+    {
+        category: "Wine Tastings",
+        id: "WT-101",
+        name: "Hastings Winery Tours and Tastings",
+        description: "Hastings Winery is a small, family owned winery in the heart of San Jose, CA. We pride ourselves on producing single vineyard, small-batch, handcrafted premium wines sourced from the finest grapes in our valley.",
+        location: "10987 FM 1187",
+        price: 12.00
+    },
+    {
+        category: "Wine Tastings",
+        id: "WT-102",
+        name: "Lone Oak Winery",
+        description: "We are a family and friend centered winery that thrives to make each of our guests feel right at home. With a growing wine list of the finest local wines, we offer tours and an incredible experience. We are open for to-go, curbside, and delivery.",
+        location: "121 Burleson Court",
+        price: 0.00
+    },
+    {
+        category: "Other",
+        id: "OTH101",
+        name: "Fire Department: Ride Along",
+        description: "Spend the day hanging out at one of our local fire stations, visiting with the staff and learning about their jobs.  If they receive a call, you can ride along with them!",
+        location: "10 Redline Drive",
+        price: 25.00
+    },
+    {
+        category: "Other",
+        id: "OTH102",
+        name: "Homes For Our Neighbors",
+        description: "Yes, you are a visitor!  But what better way to learn about a community than volunteer with the locals to build affordable housing.  Whether it be for an hour or a week, we would love to have you with us!",
+        location: "Call (555) 555-5555",
+        price: 0.00
+    }
+];
+let catList = document.getElementById("activitiesField");//houses our activities for each category
+//fields that display id, description, location, and price
+let paraId = document.getElementById("idField");
+let paraDescription = document.getElementById("descriptionField");
+let paraLocation = document.getElementById("locationField");
+let paraPrice = document.getElementById("priceField");
+let formField = document.getElementById("formField");
+catList.style.display = "none";
+formField.style.display = "none";
+
+window.onload = function () {
+    let cat = document.getElementById("categoryField");
+    createCategories(cat);
+
+    //function that acts according to category selected
+    cat.onchange = function () {
+        catList.length = 0; //clears our list of activites so they dont stack
+
+        //hides elements when category is changed
+        paraId.style.display = "none";
+        paraLocation.style.display = "none";
+        paraDescription.style.display = "none";
+        paraPrice.style.display = "none";
+        formField.style.display = "none";
+        let option;
+        switch (cat.value) {
+            case "Adventures":
+                //adding default value for adventure activities
+                option = document.createElement("option");
+                option.text = "-------Select an Adventure-------";
+                catList.appendChild(option);
+
+                createActivities("Adventures");
+                break;
+
+            case "Arts & Crafts":
+                //adding default value for Arts & Crafts activities
+                option = document.createElement("option");
+                option.text = "-------Select a Arts & Crafts-------";
+                catList.appendChild(option);
+
+                createActivities("Arts & Crafts");
+                break;
+
+            case "Museums":
+                //adding default value for Museum activities
+                option = document.createElement("option");
+                option.text = "-------Select a Museum-------";
+                catList.appendChild(option);
+
+                createActivities("Museums");
+                break;
+
+            case "Wine Tastings":
+                //adding default value for wine tasting activities
+                option = document.createElement("option");
+                option.text = "-------Select a Wine Tasting-------";
+                catList.appendChild(option);
+
+                createActivities("Wine Tastings");
+                catList.onchange = function () {
+                    console.log(catList.value);
+                }
+                break;
+
+            case "Other":
+                //adding default value for other activities
+                option = document.createElement("option");
+                option.text = "-------Select one of our other Activites-------";
+                catList.appendChild(option);
+
+                createActivities("Other");
+                break;
+
+            default:
+                createActivities("");
+                break;
+        }
+    }
+
+    //displays description/ID/Location/Price
+    catList.onchange = function () {
+        for (let i = 0; i < catList.length; i++) {
+            if (catList.value == catList[i].value) { //checks if current selected value is equal to the value in list
+                paraId.innerHTML = "ID: " + catList[i].id;
+                paraDescription.innerHTML = "Description: " + catList[i].description;
+                paraLocation.innerHTML = "Location: " + catList[i].location;
+                paraPrice.innerHTML = "Price: $" + catList[i].price;
+                paraLocation.style.display = "block";
+                paraDescription.style.display = "block";
+                paraPrice.style.display = "block";
+                paraId.style.display = "block";
+                if (activities[i].price > 0) {
+                    formField.style.display = "block";
+                    //when purchase button is clicked
+                    document.getElementById("subBtn").onsubmit = function(){
+                        //hides elements when category is changed
+                        paraId.style.display = "none";
+                        paraLocation.style.display = clear
+                        paraDescription.style.display = clear;
+                        paraPrice.style.display = clear;
+                        formField.style.display = clear;
+
+                        return false;
+                    }
+                }
+                break;
+            }
+        }
+    };
+    
+
+}
+//function that creates our categories from our array and inputs in html file
+function createCategories(cat) {
+    //create Select a Category
+    let option = document.createElement("option");
+    option.text = "-------Select a Category-------";
+    cat.appendChild(option);
+
+    for (let i = 0; i < categories.length; i++) {
+        let option = document.createElement("option");
+        option.text = categories[i];
+        cat.appendChild(option);
+    }
+}
+
+//creats activities list based on switch statement result
+function createActivities(typeOfCategory) {
+    if (typeOfCategory == "") {
+        catList.style.display = "none";
+    }
+    else {
+
+        for (let i = 0; i < activities.length; i++) {
+            if (activities[i].category == typeOfCategory) {
+                let option = document.createElement("option");
+                option.text = activities[i].name;
+                option.id = activities[i].id;
+                option.description = activities[i].description;
+                option.location = activities[i].location;
+                option.price = activities[i].price;
+
+                catList.appendChild(option);
+                catList.style.display = "inline-block";
+            }
+        }
+    }
+}
